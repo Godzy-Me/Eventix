117541e678d1d6c3f40dbc5391c1123aab0c1179
Godzy_Me <Pradnesh.khunte@gmail.com>
2026-07-16 17:08:47 +0530

    basic routing

--- bms-frontend/src/App.jsx
+++ bms-frontend/src/App.jsx
@@ -1,12 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      <div>
-        <main>
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-grow">
           <Routes>
-            {                }
             <Route path="/" element={<h1>Home Page</h1>} />
             <Route path="/profile/:id" element={<h1>Profile Page</h1>} /> 
             <Route path="/movies" element={<h1>Movies Page</h1>} />
