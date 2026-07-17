bdc9abd68f0db96f9d7ff973bf114cc65f9f8eb7
Godzy_Me <Pradnesh.khunte@gmail.com>
2026-07-17 23:32:21 +0530

    Frontend UI

--- bms-frontend/src/App.jsx
+++ bms-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/shared/Header";
 import Footer from "./components/shared/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
       <main className="flex-1">
         <Routes>
-          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/" element={<Home/>} />
           <Route path="/profile/:id" element={<h1>Profile Page</h1>} />
           <Route path="/movies" element={<h1>Movies Page</h1>} />
         </Routes>
