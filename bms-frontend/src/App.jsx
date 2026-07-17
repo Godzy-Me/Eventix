e53e25a8a175179df5c8c03352ae5fd2900eae5d
Godzy_Me <Pradnesh.khunte@gmail.com>
2026-07-17 19:31:18 +0530

    Header UI

--- bms-frontend/src/App.jsx
+++ bms-frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/shared/Header";
+import Footer from "./components/shared/Footer";
 
 function App() {
 
@@ -6,11 +8,13 @@ function App() {
     <>
       <div className="flex flex-col min-h-screen">
         <main className="flex-grow">
+          <Header />
           <Routes>
             <Route path="/" element={<h1>Home Page</h1>} />
             <Route path="/profile/:id" element={<h1>Profile Page</h1>} /> 
             <Route path="/movies" element={<h1>Movies Page</h1>} />
           </Routes>
+          <Footer />
         </main>
       </div>
     </>
