b6026c6befdcfcb09dca03203bde89a2b2b67d39
Godzy_Me <Pradnesh.khunte@gmail.com>
2026-07-18 17:54:59 +0530

    Movie Details UI

--- bms-frontend/src/App.jsx
+++ bms-frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/shared/Header";
 import Footer from "./components/shared/Footer";
 import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
 
 function App() {
   return (
@@ -10,9 +12,10 @@ function App() {
 
       <main className="flex-1">
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/profile/:id" element={<h1>Profile Page</h1>} />
-          <Route path="/movies" element={<h1>Movies Page</h1>} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
         </Routes>
       </main>
 
